71216b078af5e782f2706a2ef4ec3b5d4ea20220
ungureanudaniel <ungureanu.daniel86@gmail.com>
2021-05-22 01:23:47 +0300

    Restored all views, urls, forms back

--- photo_portfolio_app/admin.py
+++ photo_portfolio_app/admin.py
@@ -1,28 +1,28 @@
-# from django.contrib import admin
-# from .models import About, Skill, Comment, Photo, Category
-# # Register your models here.
-# class PhotoAdmin(admin.ModelAdmin):
-#     list_display = ('image_title', 'description', 'category', 'date_taken', 'category', 'tags', 'featured')
-#
-# class CategoryAdmin(admin.ModelAdmin):
-#     list_display = ('id', 'name', 'text', 'slug')
-#
-# class AboutAdmin(admin.ModelAdmin):
-#     list_display = ('title', 'description', 'thumbnail', 'headshot')
-#
-# class SkillAdmin(admin.ModelAdmin):
-#     list_display = ('skill', 'percentage')
-#
-# class CommentAdmin(admin.ModelAdmin):
-#     list_display = ('name', 'email', 'text', 'date_created', 'approved')
-#     list_filter = ('approved', 'date_created')
-#     search_fields = ('name', 'email', 'text')
-#     actions = ['approve_comments']
-#
-#
-# admin.site.register(Photo, PhotoAdmin)
-# admin.site.register(Comment, CommentAdmin)
-# # admin.site.register(Tag)
-# admin.site.register(About, AboutAdmin)
-# admin.site.register(Category, CategoryAdmin)
-# admin.site.register(Skill, SkillAdmin)
+from django.contrib import admin
+from .models import About, Skill, Comment, Photo, Category
+# Register your models here.
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ('image_title', 'description', 'category', 'date_taken', 'category', 'tags', 'featured')
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'text', 'slug')
+
+class AboutAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'thumbnail', 'headshot')
+
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ('skill', 'percentage')
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'text', 'date_created', 'approved')
+    list_filter = ('approved', 'date_created')
+    search_fields = ('name', 'email', 'text')
+    actions = ['approve_comments']
+
+
+admin.site.register(Photo, PhotoAdmin)
+admin.site.register(Comment, CommentAdmin)
+# admin.site.register(Tag)
+admin.site.register(About, AboutAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Skill, SkillAdmin)
--- photo_portfolio_app/forms.py
+++ photo_portfolio_app/forms.py
@@ -1,78 +1,78 @@
-# from .models import About, Skill, Comment, Photo, Category
-# from django import forms
-# import datetime
+from .models import About, Skill, Comment, Photo, Category
+from django import forms
+import datetime
+
+#---------------------------------GRAB CATEGORIES FROM DATABASE ----------------
+choices = Category.objects.all().values_list('name', 'name')
+
+choices_list = []
+
+for item in choices:
+    choices_list.append(item)
+
+
+
+# ------------------------ADD CATEGORY FORM -----------------------------------
+class AddSpecialtyForm(forms.ModelForm):
+
+    class Meta:
+        model = Category
+        fields = ['name', 'text', 'thumbnail', 'specialties']
+        slug = forms.CharField(widget=forms.HiddenInput(), initial="value")
+        widgets = {
+            'name': forms.TextInput(attrs = {'class': 'form-control', 'placeholder': 'Add a name...'}),
+            'text': forms.TextInput(attrs = {'class': 'form-control', 'placeholder': 'Add a description...'}),
+        }
+
+
+# #--------------------------------------------ADD PHOTO FORM -----------------------------------------------------------
+class AddPhotoForm(forms.ModelForm):
+
+    class Meta:
+        model = Photo
+        fields = ['image', 'image_title', 'description', 'category', 'tags', 'featured', 'slug']
+
+        widgets = {
+            'image_title': forms.TextInput(attrs = {'class': 'form-control', 'placeholder': 'Add a name...'}),
+            'description': forms.TextInput(attrs = {'class': 'form-control', 'placeholder': 'Add a description...'}),
+            # 'date_taken': forms.DateTime(),
+            'category': forms.Select(choices=choices_list, attrs = {'class': 'form-control'}),
+        }
+
+#-----------------------------ADD SKILLS FORM ---------------------------------
+class AddSkillForm(forms.ModelForm):
+
+    class Meta:
+        model = Skill
+        fields = ['skill', 'percentage']
+
+        widgets = {
+            'skill': forms.TextInput(attrs = {'class': 'form-control', 'placeholder': 'Add a skill...'}),
+            # 'date_taken': forms.DateTime(),
+            'percentage': forms.NumberInput(attrs = {'class': 'form-control'}),
+        }
 #
-# #---------------------------------GRAB CATEGORIES FROM DATABASE ----------------
-# choices = Category.objects.all().values_list('name', 'name')
-#
-# choices_list = []
-#
-# for item in choices:
-#     choices_list.append(item)
-#
-#
-#
-# # ------------------------ADD CATEGORY FORM -----------------------------------
-# class AddSpecialtyForm(forms.ModelForm):
-#
-#     class Meta:
-#         model = Category
-#         fields = ['name', 'text', 'thumbnail', 'specialties']
-#         slug = forms.CharField(widget=forms.HiddenInput(), initial="value")
-#         widgets = {
-#             'name': forms.TextInput(attrs = {'class': 'form-control', 'placeholder': 'Add a name...'}),
-#             'text': forms.TextInput(attrs = {'class': 'form-control', 'placeholder': 'Add a description...'}),
-#         }
-#
-#
-# # #--------------------------------------------ADD PHOTO FORM -----------------------------------------------------------
-# class AddPhotoForm(forms.ModelForm):
-#
-#     class Meta:
-#         model = Photo
-#         fields = ['image', 'image_title', 'description', 'category', 'tags', 'featured', 'slug']
-#
-#         widgets = {
-#             'image_title': forms.TextInput(attrs = {'class': 'form-control', 'placeholder': 'Add a name...'}),
-#             'description': forms.TextInput(attrs = {'class': 'form-control', 'placeholder': 'Add a description...'}),
-#             # 'date_taken': forms.DateTime(),
-#             'category': forms.Select(choices=choices_list, attrs = {'class': 'form-control'}),
-#         }
-#
-# #-----------------------------ADD SKILLS FORM ---------------------------------
-# class AddSkillForm(forms.ModelForm):
-#
-#     class Meta:
-#         model = Skill
-#         fields = ['skill', 'percentage']
-#
-#         widgets = {
-#             'skill': forms.TextInput(attrs = {'class': 'form-control', 'placeholder': 'Add a skill...'}),
-#             # 'date_taken': forms.DateTime(),
-#             'percentage': forms.NumberInput(attrs = {'class': 'form-control'}),
-#         }
-# #
-# #-----------------------------ADD ABOUT ME FORM ------------------------------
-# # class AddAboutForm(forms.ModelForm):
-# #
-# #     class Meta:
-# #         model = About
-# #         fields = ['title', 'description', 'thumbnail']
-# #
-# #         widgets = {
-# #             'title': forms.TextInput(attrs = {'class': 'form-control', 'placeholder': 'Add a salute...'}),
-# #             'description': forms.TextInput(attrs = {'class': 'form-control', 'placeholder': 'Add a description about yourself...'}),
-# #         }
-# #
-# # #--------------ADD COMMENT FORM --------------------------------------------
-# class CommentForm(forms.ModelForm):
+#-----------------------------ADD ABOUT ME FORM ------------------------------
+# class AddAboutForm(forms.ModelForm):
 #
 #     class Meta:
-#         model = Comment
-#         fields = ['name', 'email', 'text']
+#         model = About
+#         fields = ['title', 'description', 'thumbnail']
 #
 #         widgets = {
-#             'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Add your name...'}),
-#             'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Add your email address...'}),
-#             'text': forms.TextInput(attrs={'class': 'form-control', 'placeholder': '...'})
+#             'title': forms.TextInput(attrs = {'class': 'form-control', 'placeholder': 'Add a salute...'}),
+#             'description': forms.TextInput(attrs = {'class': 'form-control', 'placeholder': 'Add a description about yourself...'}),
 #         }
+#
+# #--------------ADD COMMENT FORM --------------------------------------------
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ['name', 'email', 'text']
+
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Add your name...'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Add your email address...'}),
+            'text': forms.TextInput(attrs={'class': 'form-control', 'placeholder': '...'})
+        }
