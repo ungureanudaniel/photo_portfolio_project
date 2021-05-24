9eb992603c76098e1f409fefdc16609e0d6543e2
ungureanudaniel <ungureanu.daniel86@gmail.com>
2021-05-24 16:40:09 +0300

    Fixed commented model error

--- photo_portfolio_app/models.py
+++ photo_portfolio_app/models.py
@@ -36,57 +36,57 @@ class Skill(models.Model):
         return self.skill
 
 #----------------------PHOTOS CATEGORY MODEL -----------------------------------
-# class Category(models.Model):
-#     name = models.CharField(max_length=200)
-#     slug = models.SlugField(unique=True, max_length=200, default="")
-#     text = models.TextField()
-#     thumbnail = models.ImageField(upload_to = "static/photo_portfolio_app/categ_images/", default= "")
-#     specialties = models.BooleanField(blank=True, default=False)
-#
-#     def save(self, *args, **kwargs):
-#         self.slug = slugify(self.name)
-#         super(Category, self).save(*args, **kwargs)
-#
-#     def __str__(self):
-#         return self.slug
-#
-#
-#
-#     def get_absolute_url(self):
-#         return reverse('home')
-#
-#     @property
-#     def image_url(self):
-#         if self.thumbnail and hasattr(self.thumbnail, 'url'):
-#             return self.thumbnail.url
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, max_length=200, default="")
+    text = models.TextField()
+    thumbnail = models.ImageField(upload_to = "static/photo_portfolio_app/categ_images/", default= "")
+    specialties = models.BooleanField(blank=True, default=False)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.slug
+
+
+
+    def get_absolute_url(self):
+        return reverse('home')
+
+    @property
+    def image_url(self):
+        if self.thumbnail and hasattr(self.thumbnail, 'url'):
+            return self.thumbnail.url
 
 #----------------------PHOTOS UPLOAD MODEL -----------------------------------
 
-# class Photo(models.Model):
-#     image = models.ImageField(upload_to = "images/", default="")
-#     image_title = models.CharField('A short title, max two words', max_length=200)
-#     description = models.TextField('A short description...', max_length=500, default="")
-# #     # author = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
-#     date_taken = models.DateField(auto_now_add=True)
-#     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='photo', default="")
-#     tags = TaggableManager()
-#     slug = models.SlugField(unique=True, max_length=100, default="")
-#     featured = models.BooleanField('Check this if you want picture to be shown as featured on the home page', default=False)
-#
-#     def save(self, *args, **kwargs):
-#         self.slug = slugify(self.image_title)
-#         super(Photo, self).save(*args, **kwargs)
-#
-#
-#
-#     class Meta:
-#         ordering = ["-date_taken"]
-#
-#     def get_absolute_url(self):
-#         return reverse('home')
-#
-#     def __str__(self):
-#         return self.image_title
+class Photo(models.Model):
+    image = models.ImageField(upload_to = "images/", default="")
+    image_title = models.CharField('A short title, max two words', max_length=200)
+    description = models.TextField('A short description...', max_length=500, default="")
+#     # author = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+    date_taken = models.DateField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='photo', default="")
+    tags = TaggableManager()
+    slug = models.SlugField(unique=True, max_length=100, default="")
+    featured = models.BooleanField('Check this if you want picture to be shown as featured on the home page', default=False)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.image_title)
+        super(Photo, self).save(*args, **kwargs)
+
+
+
+    class Meta:
+        ordering = ["-date_taken"]
+
+    def get_absolute_url(self):
+        return reverse('home')
+
+    def __str__(self):
+        return self.image_title
 
 # --------------------PHOTOS COMMENTS MODEL -----------------------------------
 class Comment(models.Model):
