de2c626674c120721abdfe9437be542be4e29cd9
ungureanudaniel <ungureanu.daniel86@gmail.com>
2021-05-24 16:16:49 +0300

    uploaded static files to git for website deployment on a hosting

--- photo_portfolio_app/admin.py
+++ photo_portfolio_app/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from .models import About, Skill, Comment, Photo, Category
+from .models import About, Skill, Comment
+# , Photo, Category
 # Register your models here.
-class PhotoAdmin(admin.ModelAdmin):
-    list_display = ('image_title', 'description', 'category', 'date_taken', 'category', 'tags', 'featured')
-
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'text', 'slug')
+# class PhotoAdmin(admin.ModelAdmin):
+#     list_display = ('image_title', 'description', 'category', 'date_taken', 'category', 'tags', 'featured')
+#
+# class CategoryAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'name', 'text', 'slug')
 
 class AboutAdmin(admin.ModelAdmin):
     list_display = ('title', 'description', 'thumbnail', 'headshot')
@@ -20,9 +21,9 @@ class CommentAdmin(admin.ModelAdmin):
     actions = ['approve_comments']
 
 
-admin.site.register(Photo, PhotoAdmin)
+# admin.site.register(Photo, PhotoAdmin)
 admin.site.register(Comment, CommentAdmin)
 # admin.site.register(Tag)
 admin.site.register(About, AboutAdmin)
-admin.site.register(Category, CategoryAdmin)
+# admin.site.register(Category, CategoryAdmin)
 admin.site.register(Skill, SkillAdmin)
