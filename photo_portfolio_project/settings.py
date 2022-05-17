715ec8c29b3c7acf03de138d8af7977a2d7cedaa
ungureanudaniel <ungureanu.daniel86@gmail.com>
2022-05-17 18:48:49 +0300

    Updated settings database

--- photo_portfolio_project/settings.py
+++ photo_portfolio_project/settings.py
@@ -91,7 +91,7 @@ if DEV == True:
 else:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite',
+            'ENGINE': 'django.db.backends.mysql',
             'NAME': os.getenv('DATABASE_NAME'),
             'USER': os.getenv('DATABASE_USER'),
             'PASSWORD': os.getenv('DATABASE_PASSWORD'),
