394d16aa221790efc56b802cbd9e539d419fb8f6
ungureanudaniel <ungureanu.daniel86@gmail.com>
2022-05-19 14:22:10 +0300

    Update on database selection in the development version

--- photo_portfolio_project/settings.py
+++ photo_portfolio_project/settings.py
@@ -175,4 +175,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # import dj_database_url
 # prod_db  =  dj_database_url.config(conn_max_age=500)
 # DATABASES['default'].update(prod_db)
-MAINTENANCE_MODE = True
